33f8e728571ce9d3287ec393b4a6caaf8da3f19c
Lewis Booth <lewis@LEWIS-RENDER.localdomain>
2020-11-24 16:58:51 +0000

    Added Mexi page

--- src/js/instafeed.js
+++ src/js/instafeed.js
@@ -8,5 +8,3 @@ var feed = new Instafeed({
   `
 })
 feed.run()
-
-curl - i - X GET "https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=IGQVJWUzIxVkdWR2ZA6RHNhaXZA6ZATB4ZATgxUFRKaEpsaHliaU9NZA3VGVGZAxcHZAnWi0zemR6d3N5bFdjS0J6V21qcUVVcGVNaHhKR3M4YkJvSDBWS0JJdjlrS2d6V1hwYlhGTUZArZA3ln"
